f290b1be08ad327c0602ddbca7aa966651687e9f
yueshuaipeng <yueshuaipeng@cecbrain.com>
2020-11-16 16:43:38 +0800

    avatar

--- user/user-api/src/main/java/com/lordy/user/user_api/api/AvatarService.java
+++ user/user-api/src/main/java/com/lordy/user/user_api/api/AvatarService.java
@@ -1,4 +1,10 @@
 package com.lordy.user.user_api.api;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface AvatarService {
+
+    int insertOrUpdateAvatar(MultipartFile file, Integer avatarId);
+
+    boolean deleteAvatar(Integer id);
 }
--- user/user-service/src/main/java/com/lordy/user/user_service/service/AvatarServiceImpl.java
+++ user/user-service/src/main/java/com/lordy/user/user_service/service/AvatarServiceImpl.java
@@ -1,11 +1,48 @@
 package com.lordy.user.user_service.service;
 
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.lordy.commons.database.config.CommonConfig;
 import com.lordy.user.user_api.api.AvatarService;
 import com.lordy.user.user_api.entity.Avatar;
 import com.lordy.user.user_service.mapper.AvatarMapper;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
 
 @Service
 public class AvatarServiceImpl extends ServiceImpl<AvatarMapper, Avatar> implements AvatarService {
+    @Override
+    public int insertOrUpdateAvatar(MultipartFile file, Integer avatarId) {
+        if(file.getSize() < 0){
+            return -1;
+        }
+        byte[] bytes;
+        try {
+            bytes = file.getBytes();
+        }catch (Exception e){
+            e.printStackTrace();
+            return -1;
+        }
+        if(avatarId <= 0){
+            Avatar avatar = new Avatar();
+            avatar.setImg(bytes);
+            avatar.setCreateTime(CommonConfig.sdf.format(new Date()));
+            avatar.setStatus(CommonConfig.IN_USE_STATUS);
+            insert(avatar);
+            return avatar.getId();
+        }else {
+            Avatar avatar = selectById(avatarId);
+            avatar.setImg(bytes);
+            avatar.setUpdateTime(CommonConfig.sdf.format(new Date()));
+            updateById(avatar);
+            return avatarId;
+        }
+
+    }
+
+    @Override
+    public boolean deleteAvatar(Integer id) {
+        return deleteById(id);
+    }
 }
