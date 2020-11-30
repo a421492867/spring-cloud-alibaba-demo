675f0ca5b33f1242d85c0a7ad32e06ba8f6848b3
yueshuaipeng <yueshuaipeng@cecbrain.com>
2020-11-30 09:56:41 +0800

    bug fix

--- messages/producer/login-message-producer/src/main/java/com/lordy/login_producer/LoginLogProducerApplication.java
+++ messages/producer/login-message-producer/src/main/java/com/lordy/login_producer/LoginLogProducerApplication.java
@@ -3,10 +3,11 @@ package com.lordy.login_producer;
 import com.lordy.login_producer.message.LoginLogMessageProducerSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableBinding(LoginLogMessageProducerSource.class)
 public class LoginLogProducerApplication {
