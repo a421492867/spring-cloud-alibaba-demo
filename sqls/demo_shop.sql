a5b56021926775d2c08c20408f906edfed3f07a3
a421492867 <421492867@qq.com>
2020-12-06 20:05:30 +0800

    .

--- sqls/demo_shop.sql
+++ sqls/demo_shop.sql
@@ -3,22 +3,22 @@
 
  Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50717
- Source Host           : localhost
- Source Database       : demo_shop
+ Source Server Version : 50722
+ Source Host           : localhost:3306
+ Source Schema         : demo_shop
 
  Target Server Type    : MySQL
- Target Server Version : 50717
- File Encoding         : utf-8
+ Target Server Version : 50722
+ File Encoding         : 65001
 
- Date: 12/01/2020 22:01:33 PM
+ Date: 03/12/2020 10:09:06
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `coupon`
+-- Table structure for coupon
 -- ----------------------------
 DROP TABLE IF EXISTS `coupon`;
 CREATE TABLE `coupon` (
@@ -33,7 +33,7 @@ CREATE TABLE `coupon` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `goods`
+-- Table structure for goods
 -- ----------------------------
 DROP TABLE IF EXISTS `goods`;
 CREATE TABLE `goods` (
@@ -48,7 +48,7 @@ CREATE TABLE `goods` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `order`
+-- Table structure for order
 -- ----------------------------
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order` (
@@ -56,19 +56,13 @@ CREATE TABLE `order` (
   `user_id` int(64) DEFAULT NULL,
   `order_status` char(1) DEFAULT NULL,
   `pay_status` char(1) DEFAULT NULL,
-  `shipping_status` char(1) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `consigee` varchar(255) DEFAULT NULL COMMENT '收货人',
   `good_id` int(64) DEFAULT NULL,
   `good_num` int(64) DEFAULT NULL,
   `good_price` decimal(10,2) DEFAULT NULL,
-  `good_amount` decimal(10,2) DEFAULT NULL,
-  `shipping_fee` decimal(10,2) DEFAULT NULL,
-  `order_amount` decimal(10,2) DEFAULT NULL,
+  `order_total` decimal(10,2) DEFAULT NULL,
   `coupon_id` int(64) DEFAULT NULL,
   `coupon_paid` decimal(10,2) DEFAULT NULL,
   `money_paid` decimal(10,2) DEFAULT NULL,
-  `pay_amount` decimal(10,2) DEFAULT NULL,
   `create_time` varchar(255) DEFAULT NULL,
   `confirm_time` varchar(255) DEFAULT NULL,
   `pay_time` varchar(255) DEFAULT NULL,
